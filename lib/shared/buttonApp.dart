967b6de4f00a40ce150cd359bf0dfff60964ca09
JulioAsillo <u20201b482@upc.edu.pe>
2023-10-26 20:29:45 -0500

    corregir conflictos

--- lib/shared/buttonApp.dart
+++ lib/shared/buttonApp.dart
@@ -4,22 +4,22 @@ import 'package:flutter/material.dart';
 
 Widget buttonApp(String text, Function onPressed){
   return TextButton(
-    onPressed: (){
-      onPressed();
-    },
-    style: ButtonStyle(
-      backgroundColor: MaterialStateProperty.all<Color>(Colors.white70),
-      padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
-        EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+      onPressed: (){
+        onPressed();
+      },
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color>(Colors.white70),
+        padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+          EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+        ),
       ),
-    ),
-    child: Text(
-      text,
-      style: TextStyle(
-        fontSize: 18.0,
-        fontWeight: FontWeight.bold,
-        color: Colors.black,
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: 18.0,
+          fontWeight: FontWeight.bold,
+          color: Colors.black,
+        ),
       ),
-    ),
-  );
-}
+    );
+ }
